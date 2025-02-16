1244abc30e79506bd6379b8539be6b1b2a7ceab9
hafizumiru <miruruhatsune@gmail.com>
2025-02-17 00:03:49 +0700

    feat(cart): add insert into cart feat(cart): add show inside cart

--- main_menu.py
+++ main_menu.py
@@ -2,14 +2,15 @@ import time
 import os
 
 from toko import Toko
-from data_base import item_toko
 
 toko = Toko
+
+
 def main():
-    
+
     while True:
+        os.system('cls')
         print("\nSelamat datang di L39's Store\n")
-        print("Ada yang bisa kami bantu:")
         print("1. Masuk Sebagai Karyawan")
         print("2. Mulai Berbelanja")
         print("0. Keluar Toko")
@@ -19,15 +20,13 @@ def main():
         if pilihan == '1':
             print("Anda memilih Opsi 1.")
         elif pilihan == '2':
-            toko.show_item(item_toko)
+            toko.menu_toko()
         elif pilihan == '0':
+            os.system('cls')
             print("Terima kasih atas kunjungannya! Kami tunggu kedatangan Anda kembali.")
             break
         else:
             print("Pilihan tidak valid, silakan coba lagi.")
-
-        time.sleep(3)
-        # os.system('cls')
 
 
 main()
--- toko.py
+++ toko.py
@@ -1,29 +1,112 @@
 
+import time
+import os
 from tabulate import tabulate
+
+from data_base import item_toko
+
+dict = item_toko
+keranjang_belanja = {}
 
 
 class Toko:
+    def menu_toko():
+        while True:
+            os.system('cls')
+            print('Daftar Barang di Toko:')
+            toko = []
+            for index, (key, value) in enumerate(dict.items()):
+                toko.append([index + 1, value['name'],
+                            value['category'], value['price'], value['stock']])
+            print(tabulate(toko, headers=[
+                  'No', 'Nama', 'Kategori', 'Harga', 'Stok'], tablefmt='grid'))
 
-    def show_item(dict):
-        print('Daftar Barang di Toko:')
-        toko = []
-        for index, (key, value) in enumerate(dict.items()):
-            toko.append([index + 1, value['name'], value['category'],
-                        value['price'], value['stock']])
-        print(tabulate(toko, headers=[
-              'No', 'Nama', 'Kategori', 'Harga', 'Stok']))
+            print("\nAda yang bisa kami bantu:")
+            print("1. Lihat Item Promo")
+            print("2. Pilih Barang")
+            print("3. Cari Barang")
+            print("4. Lihat Keranjang")
+            print("5. Lakukan Pembayaran")
+            print("0. Kembali")
 
-    cart = []
+            pilihan = input("\nMasukkan pilihan Anda: ")
 
-    def masuk_keranjang(cart, dict,  target):
-        pass
+            if pilihan == '1':
+                print("Anda memilih Opsi 1.")
+            elif pilihan == '2':
+                belanja = True
+                while belanja:
+                    try:
+                        index = int(input(
+                            "\nMasukan Index Barang yang akan anda beli(Gunakan angka 0 untuk kembali): "))
+                        if index == 0:
+                            belanja = False
+                        else:
+                            jumlah = int(
+                                input("\nMasukkan banyak barang yang Anda inginkan: "))
+                            masukan_keranjang(index, jumlah)
+                    except:
+                        print(
+                            "Angka yang ada masukan tidak valid. Tolong masukan hanya angka")
 
-    def show_keranjang(cart):
-        print(tabulate(cart))
+            elif pilihan == '3':
+                print("Anda memilih Opsi 3.")
+            elif pilihan == '4':
+                keranjang = True
+                while keranjang:
+                    try:
+                        os.system('cls')
+                        lihat_keranjang()
+                        index = int(input(
+                            "\nMasukan Index Barang yang akan anda beli(Gunakan angka 0 untuk kembali): "))
+                        if index == 0:
+                            keranjang = False
+                            continue
+                    except:
+                        print(
+                            "Angka yang ada masukan tidak valid. Tolong masukan hanya angka")
+            elif pilihan == '5':
+                print("Anda memilih Opsi 5.")
+            elif pilihan == '0':
+                return False
+            else:
+                print("Pilihan tidak valid, silakan coba lagi.")
 
-    def delete_item(dict, target):
-        for index, (key, value) in enumerate(dict.items()):
-            if index == (target-1):
-                del dict[key]
-                return
-        print("Index tidak ditemukan.")
+            time.sleep(1)
+
+
+def masukan_keranjang(index, jumlah):
+    item_list = list(item_toko.values())
+
+    if index < 0 or index >= len(item_list):
+        print(f"Index {index} tidak valid.")
+    else:
+        item = item_list[index-1]
+        if jumlah > item['stock']:
+            print(
+                f"Stok tidak cukup untuk {item['name']}. Stok tersedia: {item['stock']}.")
+        else:
+            item_name = item['name']
+            if item_name in keranjang_belanja:
+                keranjang_belanja[item_name]['jumlah'] += jumlah
+            else:
+                keranjang_belanja[item_name] = {
+                    "price": item['price'],
+                    "jumlah": jumlah
+                }
+            for key, value in item_toko.items():
+                if value['name'] == item_name:
+                    item_toko[key]['stock'] -= jumlah
+            print(f"{jumlah} {item_name} berhasil ditambahkan ke keranjang.")
+
+
+def lihat_keranjang():
+    if not keranjang_belanja:
+        print("Keranjang belanja kosong.")
+    else:
+        keranjang = []
+        for index, (item_name, value) in enumerate(keranjang_belanja.items()):
+            keranjang.append([index + 1, item_name, value['price'],
+                             value['jumlah'], value['price'] * value['jumlah']])
+        print(tabulate(keranjang, headers=[
+              'No', 'Nama Item', 'Harga per Unit', 'Jumlah', 'Total Harga'], tablefmt='grid'))
