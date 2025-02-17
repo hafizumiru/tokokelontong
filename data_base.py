8dccdcdcffcefda296ec3a5e7b60ae9a4418b4e8
hafizumiru <miruruhatsune@gmail.com>
2025-02-17 16:56:06 +0700

    feat(add_item): menambahkan fitur menambah item pada user karyawan

--- data_base.py
+++ data_base.py
@@ -4,154 +4,173 @@ item_toko = {
         "category": "Food",
         "price": 15000,
         "stock": 50,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_002": {
         "name": "Mie Instan",
         "category": "Food",
         "price": 3000,
         "stock": 200,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_003": {
         "name": "Susu UHT",
         "category": "Food",
         "price": 10000,
         "stock": 80,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_004": {
         "name": "Biskuit Coklat",
         "category": "Food",
         "price": 12000,
         "stock": 120,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_005": {
         "name": "Minyak Goreng 1L",
         "category": "Food",
         "price": 20000,
         "stock": 40,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_006": {
         "name": "Gula Pasir 1kg",
         "category": "Food",
         "price": 13000,
         "stock": 75,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_007": {
         "name": "Kopi Instan",
         "category": "Food",
         "price": 8000,
         "stock": 150,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_008": {
         "name": "Sereal Sarapan",
         "category": "Food",
         "price": 35000,
         "stock": 60,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_009": {
         "name": "Nasi Instan",
         "category": "Food",
         "price": 15000,
         "stock": 90,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_010": {
         "name": "Telur Ayam 1 Lusin",
         "category": "Food",
         "price": 23000,
         "stock": 50,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_011": {
         "name": "Sabun Mandi",
         "category": "Household",
         "price": 12000,
         "stock": 100,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_012": {
         "name": "Shampo Anti Ketombe",
         "category": "Household",
         "price": 18000,
         "stock": 60,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_013": {
         "name": "Pasta Gigi",
         "category": "Household",
         "price": 10000,
         "stock": 150,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_014": {
         "name": "Detergen Bubuk",
         "category": "Household",
         "price": 25000,
         "stock": 85,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_015": {
         "name": "Tisu Toilet",
         "category": "Household",
         "price": 15000,
         "stock": 200,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_016": {
         "name": "Cairan Pembersih Lantai",
         "category": "Household",
         "price": 17000,
         "stock": 70,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_017": {
         "name": "Sabun Cuci Piring",
         "category": "Household",
         "price": 14000,
         "stock": 90,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_018": {
         "name": "Pengharum Ruangan",
         "category": "Household",
         "price": 20000,
         "stock": 60,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     },
     "item_019": {
         "name": "Sikat Gigi",
         "category": "Household",
         "price": 5000,
         "stock": 180,
-        "promo": 1
+        "is_promo": 1,
+        "is_deleted": 0
     },
     "item_020": {
         "name": "Tisu Basah",
         "category": "Household",
         "price": 12000,
         "stock": 110,
-        "promo": 0
+        "is_promo": 0,
+        "is_deleted": 0
     }
 }
+
 karyawan = [
-    {"username": "karyawan1", "password": "password123"},
-    {"username": "karyawan2", "password": "password456"},
+    {"username": "mirulhafiz", "password": "1234"}
 ]
 
 members = {
-    1001: {'name': 'John Doe'},
-    1002: {'name': 'Jane Smith'},
-    1003: {'name': 'Alice Brown'}
+    1001: {'name': 'Mirul Hafiz'}
 }
 
 nota_transaksi = {
+    # Contoh isi
     # "2025-02-17 10:00:00": {
     #     "total_harga": 30000,
     #     "item_dibeli": {
--- karyawan.py
+++ karyawan.py
@@ -4,7 +4,7 @@ import pwinput
 
 from tabulate import tabulate
 
-from data_base import nota_transaksi, karyawan, item_toko
+from data_base import nota_transaksi, karyawan, item_toko, members
 
 
 class Karyawan:
@@ -44,7 +44,7 @@ def masuk_menu(username):
         if pilihan == '1':
             lihat_item(item_toko)
         elif pilihan == '2':
-            pass
+            lihat_member(members)
         elif pilihan == '3':
             recap_penjualan(nota_transaksi)
         elif pilihan == '0':
@@ -59,24 +59,31 @@ def lihat_item(item_toko):
         print('Daftar Barang di Toko:')
         toko = []
         for index, (item_id, item_detail) in enumerate(item_toko.items()):
-            toko.append([index + 1, item_id, item_detail['name'],
-                        item_detail['category'], item_detail['price'], item_detail['stock']])
+            if not item_detail['is_deleted']:
+                toko.append([item_id, item_detail['name'],
+                             item_detail['category'], item_detail['price'], item_detail['stock']])
         print(tabulate(toko, headers=[
-              'No', 'Kode Item', 'Nama', 'Kategori', 'Harga', 'Stok'], tablefmt='grid'))
+            'Kode Item', 'Nama', 'Kategori', 'Harga', 'Stok'], tablefmt='grid'))
 
         print(f"\nPilih Menu")
         print("1. Tambah Item Toko")
         print("2. Hapus Item Toko")
         print("3. Tambah Stock Item")
+        print("4. Edit Promo")
+        print("5. Restore Item yang Terhapus")
         print("0. Back")
 
         pilihan = input("\nMasukkan pilihan Anda: ")
 
         if pilihan == '1':
-            print("1")
+            tambah_item(item_toko)
         elif pilihan == '2':
-            print("2")
+            hapus_item(item_toko)
         elif pilihan == '3':
+            tambah_stok(item_toko)
+        elif pilihan == '4':
+            print("3")
+        elif pilihan == '5':
             print("3")
         elif pilihan == '0':
             return False
@@ -84,7 +91,111 @@ def lihat_item(item_toko):
             print("Pilihan tidak valid, silakan coba lagi.")
 
 
+def tambah_item(item_toko):
+    os.system('cls')
+    print("Tambah Item Toko")
+
+    if len(item_toko) == 0:
+        kode_item_baru = "item_001"
+    else:
+        # Proses Auto Increment
+        kode_item_terakhir = sorted(item_toko.keys())[-1]
+        nomor_terakhir = int(kode_item_terakhir.split('_')[1])
+        kode_item_baru = f"item_{str(nomor_terakhir + 1).zfill(3)}"
+
+    nama = input("Masukkan Nama Item: ")
+    kategori = input("Masukkan Kategori (1 untuk Food, 2 untuk Household): ")
+    while True:
+        if kategori == "1":
+            kategori = "Food"
+            break
+        elif kategori == "2":
+            kategori == "Household"
+            break
+        else:
+            print("Masukan Angka sesuai Perintah")
+
+    harga = float(input("Masukkan Harga: "))
+    stok = int(input("Masukkan Jumlah Stok: "))
+
+    # Menambahkan item ke dalam dictionary
+    item_toko[kode_item_baru] = {
+        "name": nama,
+        "category": kategori,
+        "price": harga,
+        "stock": stok,
+        "is_promo": 0,
+        "is_deleted": 0
+    }
+    print(f"Item {nama} berhasil ditambahkan dengan kode {kode_item_baru}.")
+    return item_toko
+
+
+def hapus_item(item_toko):
+    pass
+
+
+def tambah_stok(item_toko):
+    os.system('cls')
+    print("Tambah Stok Item")
+    kode_item = input(
+        "Masukkan Kode Item yang ingin ditambah stoknya (misal: item_003): ")
+    if kode_item in item_toko:
+        stok_baru = int(input("Masukkan jumlah stok yang ingin ditambahkan: "))
+        item_toko[kode_item]['stock'] += stok_baru
+        print(f"Stok item {item_toko[kode_item]['name']} berhasil diperbarui.")
+    else:
+        print(f"Item dengan kode {kode_item} tidak ditemukan.")
+    return item_toko
+
+
+def tambah_stok(item_toko):
+    pass
+
+
+def restore_item():
+    pass
+
+
+def lihat_member(members):
+    # while True:
+    #     os.system('cls')
+    #     print('Daftar Barang di Toko:')
+    #     toko = []
+    #     for index, (item_id, item_detail) in enumerate(item_toko.items()):
+    #         toko.append([index + 1, item_id, item_detail['name'],
+    #                     item_detail['category'], item_detail['price'], item_detail['stock']])
+    #     print(tabulate(toko, headers=[
+    #           'No', 'Kode Item', 'Nama', 'Kategori', 'Harga', 'Stok'], tablefmt='grid'))
+
+    #     print(f"\nPilih Menu")
+    #     print("1. Tambah Item Toko")
+    #     print("2. Hapus Item Toko")
+    #     print("3. Tambah Stock Item")
+    #     print("4. Edit Promo")
+    #     print("5. Restore Item yang Terhapus")
+    #     print("0. Back")
+
+    #     pilihan = input("\nMasukkan pilihan Anda: ")
+
+    #     if pilihan == '1':
+    #         print("1")
+    #     elif pilihan == '2':
+    #         print("2")
+    #     elif pilihan == '3':
+    #         print("3")
+    #     elif pilihan == '4':
+    #         print("3")
+    #     elif pilihan == '5':
+    #         print("3")
+    #     elif pilihan == '0':
+    #         return False
+    #     else:
+    #         print("Pilihan tidak valid, silakan coba lagi.")
+    pass
 # Fungsi Recap Penjualan
+
+
 def recap_penjualan(nota_transaksi):
     fungsi_recap = True
     while fungsi_recap:
